ccc2dd8665cec70eccaac469074ce162c588554d
Chi-Young Jeffrey Lii <lii2@users.noreply.github.com>
2016-03-13 04:24:49 +0800

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -1,23 +1,41 @@
 #written by Chi-Young Jeffrey Lii
 import numpy as np
 
-input_ = np.array([3.0, 1.0, 0.2])
+#three vector linear model for now, make scalable later
+input_ = np.array([2.0, 1.0, 0.1])
     
 weights = np.array([1, 1, 1])
 bias = np.array([0, 0, 0])
 
-# linear modeling
 scores = input_*weights + bias
 
 def softmax(x):
-    """Compute softmax values for each sets of scores in x."""
+    #Compute softmax values for each sets of scores in x.
     denom = sum(np.exp(x))
-    
     prob = np.exp(x)/denom
-    
     return prob
 
-print(softmax(scores))
+def one_hot_encoding(x):
+    #generates the correct label for each logit
+    ind = np.argmax(x) 
+    unit_vector = np.zeros(np.size(x))
+    unit_vector[ind] = 1
+    return unit_vector
+
+def cross_entropy(logit, label):
+    #generates the cross entropy between label and logit
+    #never take the log of the label
+    return -1*sum(np.log(logit)*label)
+
+def average_training_loss(x):
+    #calculates the average_training loss of the cross entropy
+    #over the entire training set
+    pass
+
+logit = softmax(scores)
+label = one_hot_encoding(logit)
+
+print(logit, label, cross_entropy(logit,label))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt
